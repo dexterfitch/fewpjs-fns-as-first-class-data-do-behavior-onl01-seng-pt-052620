ae3584c73940d22b7defda46b4239a41c235722b
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 04:33:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const hour = parseInt(time.slice(0, 2));
+  if (hour > 12) {
+    return "Good Morning";
+  } else if (12 <= hour <= 17 )  {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
